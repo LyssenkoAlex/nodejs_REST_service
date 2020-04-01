43429dd368274969c237ae7fa9db6764c5152b03
ALEX\alex <lyssenko.alex@gmail.com>
2020-04-01 15:36:16 +0600

    feat: basic ops

--- README.md
+++ README.md
@@ -7,3 +7,27 @@ npm -init
 ## start server
 npm start
  you should get 'Express server currently running on port 4000'
+
+
+sample json
+GET
+address
+http://localhost:4000/accounts/
+
+POST
+address
+http://localhost:4000/accounts/
+body
+{
+	"id": 4,
+	"username": "davesmith",
+	"role": "admin"
+}
+
+PUT
+address
+http://localhost:4000/accounts/1
+body
+{
+	"role": "guest"
+}
--- index.js
+++ index.js
@@ -50,4 +50,37 @@ app.post(`/accounts`, (request, response) => {
     accounts.push(incomingAccount);
 
     response.json(accounts);
-})
+});
+
+app.put(`/accounts/:id`, (request, response) => {
+    const accountId = Number(request.params.id);
+    const body = request.body;
+    const account = accounts.find((account) => account.id === accountId);
+    const index = accounts.indexOf(account);
+
+    if (!account) {
+        response.status(500).send('Account not found.');
+    } else {
+        const updatedAccount = { ...account, ...body };
+
+        accounts[index] = updatedAccount;
+
+        response.send(updatedAccount);
+    }
+});
+
+app.delete(`/accounts/:id`, (request, response) => {
+    const accountId = Number(request.params.id);
+    const newAccounts = accounts.filter((account) => account.id !== accountId);
+
+    if (!newAccounts) {
+        response.status(500).send('Account not found.');
+    } else {
+        accounts = newAccounts;
+        response.send(accounts);
+    }
+});
+
+
+
+
