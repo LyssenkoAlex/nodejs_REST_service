550bd7323bbd5abff7f3b4892fa68de863fb51ed
ALEX\alex <lyssenko.alex@gmail.com>
2020-04-01 15:15:58 +0600

    feat: some basics

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # nodejs_REST_service
 nodejs_REST_service
+
+#get priject
+npm -init
+
+## start server
+npm start
+ you should get 'Express server currently running on port 4000'
---
+++ index.js
@@ -0,0 +1,53 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+const PORT = 4000;
+
+app.listen(PORT, () => console.log(`Express server currently running on port ${PORT}`));
+
+let accounts = [
+    {
+        "id": 1,
+        "username": "paulhal",
+        "role": "admin"
+    },
+    {
+        "id": 2,
+        "username": "johndoe",
+        "role": "guest"
+    },
+    {
+        "id": 3,
+        "username": "sarahjane",
+        "role": "guest"
+    }
+];
+
+app.get(`/accounts`, (request, response) => {
+    response.json(accounts);
+});
+
+app.get(`/accounts/:id`, (request, response) => {
+    const accountId = Number(request.params.id);
+    const getAccount = accounts.find((account) => account.id === accountId);
+
+    if (!getAccount) {
+        response.status(500).send('Account not found.')
+    } else {
+        response.json(getAccount);
+    }
+});
+
+app.post(`/accounts`, (request, response) => {
+    const incomingAccount = request.body;
+
+    accounts.push(incomingAccount);
+
+    response.json(accounts);
+})
