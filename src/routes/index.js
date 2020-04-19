97097f4f23b20e8a49c2d11ddab8ed875b1a16da
ALEX\alex <lyssenko.alex@gmail.com>
2020-04-20 01:44:59 +0600

    feat: db insert

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,5 @@
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = function(app, dbs) {
 
     app.get('/users', (req, res) => {
@@ -12,6 +14,54 @@ module.exports = function(app, dbs) {
             }
         })
     })
+
+    app.post('/users', (req, res) => {
+        const incomingUser = req.body;
+        incomingUser.id = uuidv4();
+
+        dbs.JERY.collection('user').insertOne(incomingUser, (err, docs) => {
+            if (err) {
+                console.log(err)
+                res.error(err)
+            } else {
+                console.log('docs', docs)
+                res.json(docs)
+            }
+        })
+
+    })
+
+
+    app.get('/boards', (req, res) => {
+        console.log('start')
+        dbs.JERY.collection('boards').find({}).toArray((err, docs) => {
+            if (err) {
+                console.log(err)
+                res.error(err)
+            } else {
+                console.log('docs', docs)
+                res.json(docs)
+            }
+        })
+    })
+
+    app.get('/tasks', (req, res) => {
+        console.log('start')
+        dbs.JERY.collection('tasks').find({}).toArray((err, docs) => {
+            if (err) {
+                console.log(err)
+                res.error(err)
+            } else {
+                console.log('docs', docs)
+                res.json(docs)
+            }
+        })
+    })
+
+
+
+
+
 
 
 
